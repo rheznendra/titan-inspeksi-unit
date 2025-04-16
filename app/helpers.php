14cc00870d209fe5e7d3c21303651cf1cd4be8c4
Rheznendra Praditya <rheznendra@gmail.com>
2025-04-16 09:46:43 +0700

    [update] label handling in asSelectArray function to use shortLabel if available

--- app/helpers.php
+++ app/helpers.php
@@ -5,7 +5,7 @@ if (!function_exists('asSelectArray')) {
     {
         return array_map(
             fn($case) => [
-                'label' => method_exists($case, 'label') ? $case->label() : $case->name,
+                'label' => method_exists($case, 'label') ? ($case->shortLabel() ?? $case->label()) : $case->name,
                 'value' => $case->value
             ],
             $enumClass
